8704da91c471a7c7fc8ee6b8f8ce86c1a34621b5
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2025-01-07 16:38:05 -0300

    feat: added models

---
+++ src/Models/Post.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPrueba3.src.Models
+{
+    public class Post
+    {
+        public required string email { get; set; }
+
+        public required string password { get; set; }
+    }
+}
